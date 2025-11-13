51c07f1b3456729ba7d5a9be71f9f25fabb5ce51
erikachuasx <erikachuasx.24@ichat.sp.edu.sg>
2025-11-13 12:22:56 +0800

    changes

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -1,14 +1,11 @@
-import { test, expect } from '@playwright/test';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { test, expect } from '@playwright/test'; 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+test('button click updates message', async ({ page }) => { 
 
-test('button click updates message', async ({ page }) => {
-  const filePath = 'file://' + path.join(__dirname, '../index.html');
-  await page.goto(filePath);
+  await page.goto('file://' + __dirname + '/../index.html'); 
 
-  await page.click('#clickMe');
-  await expect(page.locator('#message')).toHaveText('Button clicked!');
-});
+  await page.click('#clickMe'); 
+
+  await expect(page.locator('#message')).toHaveText('Button clicked!'); 
+
+}); 
