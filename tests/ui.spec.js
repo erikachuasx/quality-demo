c76df43122a8899293492c213288106f520a412b
erikachuasx <erikachuasx.24@ichat.sp.edu.sg>
2025-11-13 12:27:08 +0800

    changes last

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -1,11 +1,10 @@
-import { test, expect } from '@playwright/test'; 
+const { test, expect } = require('@playwright/test');
+const path = require('path');
 
-test('button click updates message', async ({ page }) => { 
+test('button click updates message', async ({ page }) => {
+  const filePath = 'file://' + path.join(__dirname, '../index.html');
+  await page.goto(filePath);
 
-  await page.goto('file://' + __dirname + '/../index.html'); 
-
-  await page.click('#clickMe'); 
-
-  await expect(page.locator('#message')).toHaveText('Button clicked!'); 
-
-}); 
+  await page.click('#clickMe');
+  await expect(page.locator('#message')).toHaveText('Button clicked!');
+});
